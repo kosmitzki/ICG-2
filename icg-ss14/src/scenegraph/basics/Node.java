f49da79c6f7d0d56682f4e83c012af973db41045
kosmitzki <mitch>
2014-07-02 09:21:09 +0200

    comments in node

--- icg-ss14/src/scenegraph/basics/Node.java
+++ icg-ss14/src/scenegraph/basics/Node.java
@@ -9,31 +9,34 @@ import static ogl.vecmathimp.FactoryDefault.vecmath;
 
 public abstract class Node {
 	
+	
+	// enthaellt eine liste von kindknoten
 	List<Node> childNode = new ArrayList<Node>();
 	Matrix transformation;
 	String name;
 	
+	
+	// wie bei RotatingCube -> und gleiches interface
 	public Node() {
 		setTransformation(vecmath.identityMatrix());
 	}
-	
 	public abstract void display(Matrix m);
 
+	
+	// 2 methoden zum hinzufuegen und entfernen von kindknoten
 	public List<Node> deleteChild(int position) {
 		childNode.remove(position);
 		return childNode;
 	}
-	
 	public List<Node> addChild (Node child) {
 		childNode.add(child);
 		return childNode;
 	}
 	
-
+	// getter setter machen sinn
 	public Matrix getTransformation() {
 		return transformation;
 	}
-
 	public void setTransformation(Matrix transformation) {
 		this.transformation = transformation;
 	}
@@ -41,7 +44,6 @@ public abstract class Node {
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
