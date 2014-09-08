6663d0d6ca306e1242f94a6d35f999fc6609f73e
Juliaaa <julia_barnick@hotmail.de>
2014-09-08 15:57:18 +0200

    scale

--- icg-ss14/src/Animation/Scale.java
+++ icg-ss14/src/Animation/Scale.java
@@ -2,9 +2,12 @@ package Animation;
 
 import static ogl.vecmathimp.FactoryDefault.vecmath;
 
+import javax.swing.text.Position;
+
 import org.lwjgl.input.Keyboard;
 
 import ogl.app.Input;
+import ogl.vecmath.Matrix;
 import scenegraph.basics.Node;
 
 
@@ -17,10 +20,15 @@ public class Scale extends Animation {
 
 	@Override
 	public void animate(Input input) {
+		float big = 1.01f;
+		float small = 0.99f;
 		if (input.isKeyDown(Keyboard.KEY_B)) {
-			node.setTransformation(vecmath.scaleMatrix(2, 2, 2));
+			Matrix help = node.getTransformation().mult(vecmath.scaleMatrix(big, big, big));
+			node.setTransformation(help);
 		} if (input.isKeyDown(Keyboard.KEY_S)) {
-			node.setTransformation(vecmath.scaleMatrix(0.5f, 0.5f, 0.5f));
+			Matrix help1 = node.getTransformation().mult(vecmath.scaleMatrix(small, small, small));
+			node.setTransformation(help1);
+			//reset
 		} if (input.isKeyDown(Keyboard.KEY_N)) {
 			node.setTransformation(vecmath.scaleMatrix(1, 1, 1));
 		}
