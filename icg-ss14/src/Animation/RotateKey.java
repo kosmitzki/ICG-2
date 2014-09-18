36f14c6e20940dc3d38ddadd6284b2bf7c41cc40
Juliaaa <julia_barnick@hotmail.de>
2014-09-18 15:53:22 +0200

    free fly sollte eig gehen geht aber nicht immer

--- icg-ss14/src/Animation/RotateKey.java
+++ icg-ss14/src/Animation/RotateKey.java
@@ -11,35 +11,45 @@ import scenegraph.basics.Node;
 
 public class RotateKey extends Rotate {
 
-	
+
 	int key;
 	float angle;
-	
+	float angle2;
+	boolean switchTurn = false;
+
 	//dreht irgendwas, aber laesst auch den cube verschwinden wenn 0.0.0
-	
-	
+
+
 	public RotateKey(Node node, int key, float angle) {
 		// TODO Auto-generated constructor stub
 		super(node);
 		this.key = key;
 		this.angle = angle+0.9f;
+		this.angle2 = angle-0.8f;
 	}
 
 
 
 	@Override
 	public void animate(Input input) {	
-		if (input.isKeyDown(Keyboard.KEY_X)) {
+		
+		if (input.isKeyDown(Keyboard.KEY_X) && input.isKeyDown(Keyboard.KEY_A)) {
 			animate(axisX, angle);
-		}
-		if (input.isKeyDown(Keyboard.KEY_Y)) {
-			animate(axisY, angle);
-
-			
 		} 
-		if (input.isKeyDown(Keyboard.KEY_Z)) {
+		if (input.isKeyDown(Keyboard.KEY_X) && input.isKeyDown(Keyboard.KEY_D)) {
+			animate(axisX, angle2);
+		} 
+		if (input.isKeyDown(Keyboard.KEY_Y) && input.isKeyDown(Keyboard.KEY_A)) {
+			animate(axisY, angle);
+		}  
+		if (input.isKeyDown(Keyboard.KEY_Y) && input.isKeyDown(Keyboard.KEY_D)) {
+			animate(axisY, angle2);
+		} 
+		if (input.isKeyDown(Keyboard.KEY_Z) && input.isKeyDown(Keyboard.KEY_A)) {
 			animate(axisZ, angle);
-
+		}  
+		if (input.isKeyDown(Keyboard.KEY_Z) && input.isKeyDown(Keyboard.KEY_D)) {
+			animate(axisZ, angle2);
 		}
-}}
-	
+	}}
+
